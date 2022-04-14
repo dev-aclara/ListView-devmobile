e1fcd1dabe00658097113f5e03abb76e3bd6b299
Ana Clara Mansano <3119@fai.com.br>
2022-04-13 22:13:13 -0300

    Atualizando

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener  {
     ListView lv;
@@ -21,13 +22,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         carros [2]="aaaa2" ;
         carros [3]="aaaa3";
         carros [4]="aaaa4";
+        lv = findViewById(R.id.lista);
+        lv.setOnItemClickListener(this);
         ArrayAdapter<String> adapter = new
                 ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, carros);
-
+        lv.setAdapter(adapter);
     }
 
     @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        
+    public void onItemClick(AdapterView<?> parent,
+                            View view, int position, long id) {
+        Toast.makeText(this, "teste  "+carros[position], Toast.LENGTH_SHORT).show();
+
     }
 }
